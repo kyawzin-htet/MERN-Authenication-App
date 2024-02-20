09bdaeeed2f5ba73807551f3bbacff00d499f129
Kyaw Zin Htet <kyawzinhtet@Kyaws-MacBook-Pro.local>
2024-02-20 11:10:38 +0630

    update

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-const {MongoMemoryServer} = require('mongodb-memory-server')
+// const {MongoMemoryServer} = require('mongodb-memory-server')
 require('dotenv').config();
 
 const conn = async()=>{
-    const mongod = await MongoMemoryServer.create();
-    const getUri = mongod.getUri();
+    // const mongod = await MongoMemoryServer.create();
+    // const getUri = mongod.getUri();
     mongoose.set('strictQuery', true)
-    const db = await mongoose.connect("mongodb+srv://mernauthapp:mernauthapp@emaily.bmu05.mongodb.net/mernauthapp?retryWrites=true&w=majority")
+    const db = await mongoose.connect(process.env.MONGO_DB_URL)
     // const db = await mongoose.connect(getUri);
     console.log("database connected")
     return db;
